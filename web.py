41b6541d3a6c4c370ba6a6ce850319b80cb52f6f
ismail <ismail.aboulela@gmail.com>
2024-03-25 10:08:33 +0400

    inital commit

---
+++ web.py
@@ -0,0 +1,27 @@
+import streamlit as slt
+import functions
+
+def addTodo():
+    todo = slt.session_state["todoInputBox"]+"\n"
+    todos.append(todo)
+    functions.writeTodos(todos)
+
+todos = functions.getTodos()
+
+slt.title("My Todo App")
+slt.subheader("Todos")
+slt.write("This app is to increase your productivity")
+
+#complete a todo
+for index, todo in enumerate(todos):
+    checkbox = slt.checkbox(todo, key=todo)
+    if checkbox == True:
+        todos.pop(index)
+        functions.writeTodos(todos)
+        del slt.session_state[todo]
+        slt.rerun()
+
+slt.text_input(label=" ", placeholder="enter todo here",
+               on_change=addTodo, key="todoInputBox")
+
+slt.session_state
